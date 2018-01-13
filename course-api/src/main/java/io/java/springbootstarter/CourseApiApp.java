652bbd336012110e5005c909e81ebbda79135ba8
SidharthKBurnwal <sidharth.k.burnwal@gmail.com>
2018-01-14 00:43:16 +0530

    first file

---
+++ course-api/src/main/java/io/java/springbootstarter/CourseApiApp.java
@@ -0,0 +1,15 @@
+package io.java.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*this will tell where to start*/
+@SpringBootApplication
+public class CourseApiApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiApp.class, args);
+	}
+
+}
+  
